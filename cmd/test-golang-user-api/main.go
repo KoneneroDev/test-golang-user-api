fc9ce964a69ec0e2d8c962f4ee87cb6ec61127d3
Svyatoslav Kochergin <SKochergin@alfabank.ru>
2025-05-14 19:20:28 +0300

    feat(): add logger

--- cmd/test-golang-user-api/main.go
+++ cmd/test-golang-user-api/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"log/slog"
+	"os"
 	"test_golang_user_api/internal/config"
 )
 
 func main() {
 	cfg := config.LoadConfig()
 
-	//todo: logger
+	log := slog.New(
+		slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+	)
+
+	log.Info("starting server ", slog.String("env", cfg.Env))
 
 	//todo: storage
 
